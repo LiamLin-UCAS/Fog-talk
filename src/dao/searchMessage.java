7f954fef54122d4e5d3cfe211b9aec8358e9afaa
LiuYuncong <2959747634@qq.com>
2020-12-25 14:35:16 +0800

    目前完成：登录，注册，查看全部帖子及其评论,搜索帖子

---
+++ src/dao/searchMessage.java
@@ -0,0 +1,68 @@
+package dao;
+
+import model.Comment;
+import model.Post;
+import util.JDBCUtils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import static dao.showComment.showComment;
+
+public class searchMessage
+{
+    private static Connection con;
+    private static PreparedStatement stmt;
+    private static ResultSet rs;
+
+    public static Post[] searchMessage(String message)
+    {
+        try
+        {
+            con = JDBCUtils.getConnection();
+            Post[] posts = new Post[200];
+            Post post;
+
+            String sql1 = "select * from post where content like '%" + message + "%'";
+            stmt = con.prepareStatement(sql1);
+
+            rs = stmt.executeQuery();
+
+            int number = 0;
+            while(rs.next())
+            {
+                post = new Post();
+
+                post.setPostId(rs.getString(1));
+                post.setContent(rs.getString(2));
+                post.setPublisher(rs.getString(3));
+                post.setReleaseTime(rs.getString(4));
+
+                Comment[] comments = showComment(post.getPostId());
+
+                post.setComments(comments);
+
+                int length = 0;
+                while (comments[length] != null) length++;
+
+                post.setCommentsLength(length);
+
+                posts[number] = post;
+                number++;
+            }
+
+            return posts;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            JDBCUtils.close(rs, stmt, con);
+        }
+
+        return null;
+    }
+}
